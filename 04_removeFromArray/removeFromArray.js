3c77482610f9729b2317053b7b04588be2cb9a90
STORM533 <sakshamkr2006@gmail.com>
2024-12-26 03:35:37 +0530

    q5 till done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrays , ...removeDigit) {
+    
+    const newArray = [];
+    for(let i = 0 ; i <=  arrays.length; i++){
+        if(!removeDigit.includes(arrays.at(i)) ){
+            newArray.push(arrays.at(i));
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+    let sum = 0;
+    let smallNum = 0;
+    let largeNum = 0;
+    
+    
+    if( !Number.isInteger(num1) || !Number.isInteger(num2))
+        return 'ERROR';
+    
+    if(num1  < 0 || num2 < 0)
+        return 'ERROR';
+    
+    if(num1 < num2){
+        smallNum = num1;
+        largeNum = num2;
+    }else{
+        largeNum = num1;
+        smallNum = num2;
+    }
+
+    for(let i = smallNum ; i<=largeNum ; i++){
+        
+        sum = sum  + i;
+    }
+    return sum;
 
 };
 
