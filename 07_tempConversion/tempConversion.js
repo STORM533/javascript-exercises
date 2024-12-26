bc4df91900291d9e1161554257ab3703dc312e9e
STORM533 <sakshamkr2006@gmail.com>
2024-12-26 15:35:47 +0530

    q6 and 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+celsius = (temp -32) *(5/9);
+let roundoff = Math.round(celsius *10)/10;
+return roundoff;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = (temp *(9/5)) + 32;
+  let roundoff = Math.round(fahrenheit *10)/10;
+return roundoff;
 };
 
 // Do not edit below this line
