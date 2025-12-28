3adf2dae38e25c87708309e44220e749dddf07a8
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2025-12-28 18:02:00 +0530

    done

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,7 @@
 const factorial = function() {
-
+    if (!Number.isInteger(n) || n < 0) return;
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
 };
-
 // Do not edit below this line
 module.exports = factorial;
--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,33 @@
-const contains = function() {
-  
+const contains = function (object, searchValue) {
+  //can use word
+  // const values = Object.values(object);
+  // if (values.includes(word)) return true;
+
+  // in this solution the mistake i made was that instead of checking the values inside values variable
+  // i was checking the value variables time after time. (in line 7)
+  //To fix this we will loop over values variable and check inside.
+
+  // if (typeof values === "object" && values !== null) {
+  //   if (contains(values, word)) return true;
+  //  }
+  const values = Object.values(object); //same as first line
+
+  // NaN === NaN evaluates to false
+  // Normally, we would have to do an explicit Number.isNaN() check to compare NaN equality
+  // However, Array.prototype.includes automatically handles this for us
+  if (values.includes(searchValue)) return true; //same as second line
+
+  const nestedObjects = values.filter(
+    //this is where we iterate over the values variables inside
+    // typeof null === 'object' evaluates to true ¯\_(ツ)_/¯
+    (value) => typeof value === "object" && value !== null
+  );
+  //the some  keyword instead of iterating one by one goes throught all the values inside
+  //and sees which value passes the condition
+  return nestedObjects.some((nestedObject) =>
+    contains(nestedObject, searchValue)
+  );
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
